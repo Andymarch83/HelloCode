6be9e188aec67b101443d7b2509aba2e2e161e10
Andrey <5657031@gmail.com>
2023-01-26 22:08:07 +0300

    Fix changes

---
+++ Exemple007_Magic/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+// Console.SetCursorPosition(10, 1);
+// Console.WriteLine("+");
+
+int xa = 40, ya = 1,
+    xb = 1,  yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
