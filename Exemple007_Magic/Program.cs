f1a71a91d5d6b36a7ad54d393de1d6288c45b2bc
Andrey <5657031@gmail.com>
2023-01-26 22:31:56 +0300

    Fix2

--- Exemple007_Magic/Program.cs
+++ Exemple007_Magic/Program.cs
@@ -2,7 +2,7 @@
 // Console.SetCursorPosition(10, 1);
 // Console.WriteLine("+");
 
-int xa = 40, ya = 1,
+int xa = 40, ya = 5,
     xb = 1,  yb = 30,
     xc = 80, yc = 30;
 
@@ -14,3 +14,30 @@ Console.WriteLine("+");
 
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+int count = 0;
+
+while(count < 10000) 
+{
+    int what = new Random().Next(0, 3); // [0,3] 0 1 2
+    if(what == 0)
+   {
+        x = (x+xa)/2;
+        y = (y+ya)/2;
+    }
+    if(what == 1)
+   {
+       x = (x+xb)/2;
+       y = (y+yb)/2;
+    }
+    if(what == 2)
+   {
+       x = (x+xc)/2;
+       y = (y+yc)/2;
+    }
+  Console.SetCursorPosition(x, y);
+  Console.WriteLine("+");
+  count = count++;
+}
