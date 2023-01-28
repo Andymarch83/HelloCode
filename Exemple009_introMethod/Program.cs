7a6ead8d918d34d578ea222717c03035726ca12f
Andrey <5657031@gmail.com>
2023-01-28 14:56:51 +0300

    Exemple009_introMethod

---
+++ Exemple009_introMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+     int result = arg1;
+     if(arg2>result) result = arg2;
+     if(arg3>result) result = arg3;
+     return result;
+}
+
+
+int[] array = {16,21,13,416,25,67,47,28,19};
+
+int result = Max(
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+     Max(array[6], array[7], array[8])
+);
+
+Console.WriteLine(result);
